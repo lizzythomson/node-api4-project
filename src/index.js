4823b981624c64a450882c680861a65e254245f6
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-02-10 13:34:51 -0700

    added basic setup and setup basic endpoints

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-const express = require('express');
-const server = express();
+const server = require('./server');
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
   console.log(process.env.MESSAGE);
-});
-
-server.use('*', (req, res) => {
-  res.send('<h1>Hello, Heroku!</h1>');
 });
--- src/server.js
+++ src/server.js
@@ -1,6 +1,49 @@
-// GET	/api/users
-// Returns an array users.
-// POST	/api/register
-// Creates a user from { username, password } in the request body, responds with newly created user.
-// POST	/api/login
-// Checks { username, password } in the request body, responds with a welcome message.
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const users = [
+  {
+    username: 'Adriana',
+    password: 'security123',
+  },
+  {
+    username: 'Barry',
+    password: 'password123',
+  },
+  {
+    username: 'Candice',
+    password: 'authenicate123',
+  },
+];
+
+server.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+server.post('/api/register', (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({ message: 'username and password are required' });
+  } else {
+    users.push(req.body);
+    res.json(users);
+  }
+});
+
+server.post('/api/login', async (req, res) => {
+  const verifyUsername = await users.find(req.body.username);
+  const verifyPassword = await users.find(req.body.password);
+  if (!verifyUsername || !verifyPassword) {
+    res.status(400).json({ message: 'username and password are not correct' });
+  } else {
+    res.json({ message: `Welcome ${req.body.username}` });
+  }
+});
+
+server.use('/', (req, res) => {
+  res.send('<h1>Hello, Heroku!</h1>');
+});
+
+module.exports = server;
