7a47f17c5c867515e48a226c55db11e9a4d98283
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-02-10 11:30:22 -0700

    setup simple API

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+  console.log(process.env.MESSAGE);
+});
+
+server.use('*', (req, res) => {
+  res.send('<h1>Hello, Heroku!</h1>');
+});
