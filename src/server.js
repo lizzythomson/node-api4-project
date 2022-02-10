58f9be53244c7bc78a33c33e74d020bfc31b26fa
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-02-10 12:32:41 -0700

    updating

---
+++ src/server.js
@@ -0,0 +1,6 @@
+// GET	/api/users
+// Returns an array users.
+// POST	/api/register
+// Creates a user from { username, password } in the request body, responds with newly created user.
+// POST	/api/login
+// Checks { username, password } in the request body, responds with a welcome message.
